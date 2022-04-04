4e7220b03b546639c1cec6a98fe2d664a0bb6ad9
ROCIO PE�A <78064085+RocioPena@users.noreply.github.com>
2022-04-04 19:58:22 +0000

    personaje_meta,caja,espacio

--- sokoban.py
+++ sokoban.py
@@ -20,7 +20,7 @@ class Sokoban:
     self.mapa=[
         [3,3,3,3,3,3,3,3,3,3,3,3],
         [3,1,1,1,1,1,1,1,1,1,1,3],
-        [3,1,1,1,5,4,1,1,1,1,1,3],
+        [3,1,1,1,5,2,1,1,1,1,1,3],
         [3,1,1,1,1,1,1,1,1,1,1,3],
         [3,1,1,1,1,1,1,1,1,1,1,3],
         [3,3,3,3,3,3,3,3,3,3,3,3]
@@ -123,7 +123,17 @@ class Sokoban:
         ):  # If the character is on the floor and the next position is a floor
             self.mapa[self.personaje_fil][self.personaje_col] = self.meta  
             self.mapa[self.personaje_fil][self.personaje_col + 1] = self.personaje_meta
-            self.personaje_col = self.personaje_col + 1          
+            self.personaje_col = self.personaje_col + 1
+    #personaje_meta,caja,espacio    
+    elif (
+            self.mapa[self.personaje_fil][self.personaje_col] == self.personaje_meta
+            and self.mapa[self.personaje_fil][self.personaje_col + 1] == self.caja
+            and self.mapa[self.personaje_fil][self.personaje_col + 2] == self.espacio
+        ):  # If the character is on the floor and the next position is a floor
+            self.mapa[self.personaje_fil][self.personaje_col] = self.meta  
+            self.mapa[self.personaje_fil][self.personaje_col + 1] = self.personaje
+            self.mapa[self.personaje_fil][self.personaje_col + 2] = self.caja
+            self.personaje_col = self.personaje_col + 1                    
 
 
   def checharNivelCompleto(self):
