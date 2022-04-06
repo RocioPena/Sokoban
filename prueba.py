306c8add5d8e9f0b620777f955a0a064cf040550
ROCIO PE�A <78064085+RocioPena@users.noreply.github.com>
2022-04-06 16:51:26 +0000

    personaje,caja_meta,espacio(arriba)

---
+++ prueba.py
@@ -0,0 +1,9 @@
+class Sokoban:
+
+    mapa = []  # mapa de juego
+
+    def __init__(self):
+        pass
+
+    def moverDerecha(self):
+        print("mover")
--- sokoban.py
+++ sokoban.py
@@ -20,8 +20,8 @@ class Sokoban:
     self.mapa=[
         [3,3,3,3,3,3,3,3,3,3,3,3],
         [3,1,1,1,1,1,1,1,1,1,1,3],
-        [3,1,1,1,4,1,1,1,1,1,1,3],
-        [3,1,1,1,2,1,1,1,1,1,1,3],
+        [3,1,1,1,1,1,1,1,1,1,1,3],
+        [3,1,1,1,6,1,1,1,1,1,1,3],
         [3,1,1,1,0,1,1,1,1,1,1,3],
         [3,3,3,3,3,3,3,3,3,3,3,3]
     ]
@@ -329,7 +329,7 @@ class Sokoban:
             self.mapa[self.personaje_fil - 1][self.personaje_col] = self.personaje 
             self.mapa[self.personaje_fil - 2][self.personaje_col] = self.caja_meta
             self.personaje_fil = self.personaje_fil - 1    
-     #personaje,caja_meta,espacio     
+     #personaje,caja_meta,espacio(arriba)     
     elif (
             self.mapa[self.personaje_fil][self.personaje_col] == self.personaje
             and self.mapa[self.personaje_fil - 1][self.personaje_col] == self.caja_meta
