8031ff7dfd9f231aafb90d03b2cde2b234f88dd3
ROCIO PE�A <78064085+RocioPena@users.noreply.github.com>
2022-04-06 16:59:31 +0000

    personaje_meta,espacio(arriba),personaje_meta,meta(arriba)

--- sokoban.py
+++ sokoban.py
@@ -20,9 +20,9 @@ class Sokoban:
     self.mapa=[
         [3,3,3,3,3,3,3,3,3,3,3,3],
         [3,1,1,1,1,1,1,1,1,1,1,3],
-        [3,1,1,1,4,1,1,1,1,1,1,3],
-        [3,1,1,1,6,1,1,1,1,1,1,3],
-        [3,1,1,1,0,1,1,1,1,1,1,3],
+        [3,1,1,1,1,1,1,1,1,1,1,3],
+        [3,1,1,1,1,4,1,1,1,1,1,3],
+        [3,1,1,1,5,4,1,1,1,1,1,3],
         [3,3,3,3,3,3,3,3,3,3,3,3]
     ]
 #esto firve para leer un archivo de texto
@@ -349,31 +349,31 @@ class Sokoban:
             self.mapa[self.personaje_fil - 1][self.personaje_col] = self.personaje_meta 
             self.mapa[self.personaje_fil - 2][self.personaje_col] = self.caja_meta
             self.personaje_fil = self.personaje_fil - 1 
-    #personaje_meta,caja,espacio      
+    #personaje_meta,caja,espacio(arriba)      
     elif (
             self.mapa[self.personaje_fil][self.personaje_col] == self.personaje_meta
-            and self.mapa[self.personaje_fil][self.personaje_col + 1] == self.personaje
-            and self.mapa[self.personaje_fil][self.personaje_col + 2] == self.espacio
+            and self.mapa[self.personaje_fil - 1][self.personaje_col] == self.personaje
+            and self.mapa[self.personaje_fil - 2][self.personaje_col] == self.espacio
         ):  # If the character is on the floor and the next position is a floor
             self.mapa[self.personaje_fil][self.personaje_col] = self.meta  
-            self.mapa[self.personaje_fil][self.personaje_col + 1] = self.personaje
-            self.mapa[self.personaje_fil][self.personaje_col + 2] = self.caja
+            self.mapa[self.personaje_fil - 1][self.personaje_col] = self.personaje
+            self.mapa[self.personaje_fil - 2][self.personaje_col] = self.caja
             self.personaje_fil = self.personaje_fil - 1                     
-    #personaje_meta,espacio     
+    #personaje_meta,espacio(arriba)    
     elif (
             self.mapa[self.personaje_fil][self.personaje_col] == self.personaje_meta
-            and self.mapa[self.personaje_fil][self.personaje_col + 1] == self.espacio
+            and self.mapa[self.personaje_fil - 1][self.personaje_col] == self.espacio
         ):  # If the character is on the floor and the next position is a floor
             self.mapa[self.personaje_fil][self.personaje_col] = self.meta  
-            self.mapa[self.personaje_fil][self.personaje_col + 1] = self.personaje
+            self.mapa[self.personaje_fil - 1][self.personaje_col] = self.personaje
             self.personaje_fil = self.personaje_fil - 1  
-    #personaje_meta,meta     
+    #personaje_meta,meta(arriba)     
     elif (
             self.mapa[self.personaje_fil][self.personaje_col] == self.personaje_meta
-            and self.mapa[self.personaje_fil][self.personaje_col + 1] == self.meta
+            and self.mapa[self.personaje_fil - 1][self.personaje_col] == self.meta
         ):  # If the character is on the floor and the next position is a floor
             self.mapa[self.personaje_fil][self.personaje_col] = self.meta  
-            self.mapa[self.personaje_fil][self.personaje_col + 1] = self.personaje_meta
+            self.mapa[self.personaje_fil - 1][self.personaje_col] = self.personaje_meta
             self.personaje_fil = self.personaje_fil - 1
     #personaje_meta,caja,espacio    
     elif (
