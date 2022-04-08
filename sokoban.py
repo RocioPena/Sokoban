bd4e17ef876bd7d1be3a2ddfdd182754d985689e
ROCIO PE�A <78064085+RocioPena@users.noreply.github.com>
2022-04-08 16:28:25 +0000

    personaje_meta,caja_meta,espacio(arriba)

--- sokoban.py
+++ sokoban.py
@@ -20,8 +20,8 @@ class Sokoban:
     self.mapa=[
         [3,3,3,3,3,3,3,3,3,3,3,3],
         [3,1,1,1,1,1,1,1,1,1,1,3],
-        [3,1,1,1,4,1,1,1,1,1,1,3],
-        [3,1,1,1,2,4,1,1,1,1,1,3],
+        [3,1,1,1,1,1,1,1,1,1,1,3],
+        [3,1,1,1,6,4,1,1,1,1,1,3],
         [3,1,1,1,5,4,1,1,1,1,1,3],
         [3,3,3,3,3,3,3,3,3,3,3,3]
     ]
@@ -395,15 +395,15 @@ class Sokoban:
             self.mapa[self.personaje_fil - 1][self.personaje_col] = self.personaje
             self.mapa[self.personaje_fil - 2][self.personaje_col] = self.caja_meta
             self.personaje_fil = self.personaje_fil - 1    
-    #personaje_meta,caja_meta,espacio   
+    #personaje_meta,caja_meta,espacio(arriba)   
     elif (
             self.mapa[self.personaje_fil][self.personaje_col] == self.personaje_meta
-            and self.mapa[self.personaje_fil][self.personaje_col + 1] == self.caja_meta
-            and self.mapa[self.personaje_fil][self.personaje_col + 2] == self.espacio
+            and self.mapa[self.personaje_fil - 1][self.personaje_col] == self.caja_meta
+            and self.mapa[self.personaje_fil - 2][self.personaje_col] == self.espacio
         ):  # If the character is on the floor and the next position is a floor
             self.mapa[self.personaje_fil][self.personaje_col] = self.meta  
-            self.mapa[self.personaje_fil][self.personaje_col + 1] = self.personaje_meta
-            self.mapa[self.personaje_fil][self.personaje_col + 2] = self.caja
+            self.mapa[self.personaje_fil - 1][self.personaje_col] = self.personaje_meta
+            self.mapa[self.personaje_fil - 2][self.personaje_col] = self.caja
             self.personaje_fil = self.personaje_fil - 1
     #personaje_meta,caja_meta,meta   
     elif (
